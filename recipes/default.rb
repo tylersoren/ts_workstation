3c5256de249f109c6173189a415d121358a50cd5
Tyler Sorensen <tyler.sorensen@cellularsales.com>
2019-08-12 09:05:24 -0400

    added git config

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,13 +5,20 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 include_recipe 'ts_workstation::apps'
-include_recipe 'ts_workstation::tasks'
+
+# only run once to create the tasks but need to manually change to local user and only run when logged on
+# include_recipe 'ts_workstation::tasks'
 
 directory 'C:\\repos' do
   action :create
 end
 
 directory 'C:\\temp' do
+  action :create
+end
+
+cookbook_file 'C:\\Users\tyler.sorensen\.gitconfig' do
+  source '.gitconfig'
   action :create
 end
 
